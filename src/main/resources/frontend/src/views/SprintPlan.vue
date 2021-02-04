d00adc169def373babff1bf7189e1c4d7ee4d256
andrew <andrii.skrypnik@gmail.com>
2021-02-04 12:17:02 +0200

    Added sprint plan page content

--- src/main/resources/frontend/src/views/SprintPlan.vue
+++ src/main/resources/frontend/src/views/SprintPlan.vue
@@ -1,7 +1,31 @@
 <template>
-  <h1>
-    Sprint plan
-  </h1>
+  <div>
+    <h1>
+      Sprint plan
+    </h1>
+    <p>Backend</p>
+    <ul>
+      <li>Create database structure <span>&#10003;</span></li>
+      <li>Create domain models and map them to database records <span>&#10003;</span></li>
+      <li>Create login functionality <span>&#10003;</span></li>
+      <li>Create CRUD endpoints snd services for book</li>
+      <li>Create user registration endpoint</li>
+      <li>Create book order functionality</li>
+      <li>Create book search with filtration functionality</li>
+    </ul>
+    <p/>
+    <hr>
+    <p>Frontend</p>
+    <ul>
+      <li>Create login functionality <span>&#10003;</span></li>
+      <li>Create sprint-plan page <span>&#10003;</span></li>
+      <li>Create web-site prototype</li>
+      <li>Create user registration form</li>
+      <li>Create CRUD functionality for books</li>
+      <li>Create order page</li>
+      <li>Create book search with filtration functionality</li>
+    </ul>
+  </div>
 </template>
 
 <script>
